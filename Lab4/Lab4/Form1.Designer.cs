d074adeae8c672d98c7b968f72b4632a47ac117b
VuNgocDuyKhanh <102215602+VuNgocDuyKhanh@users.noreply.github.com>
2023-04-06 15:06:09 +0700

    Add files via upload

---
+++ Lab4/Lab4/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Lab4
+{
+    partial class ConwayMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbGrid = new System.Windows.Forms.PictureBox();
+            this.numSSize = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.btnAdvance = new System.Windows.Forms.Button();
+            this.cboCells = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbGrid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numSSize)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbGrid
+            // 
+            this.pbGrid.Location = new System.Drawing.Point(12, 12);
+            this.pbGrid.Name = "pbGrid";
+            this.pbGrid.Size = new System.Drawing.Size(1058, 351);
+            this.pbGrid.TabIndex = 0;
+            this.pbGrid.TabStop = false;
+            // 
+            // numSSize
+            // 
+            this.numSSize.Location = new System.Drawing.Point(80, 370);
+            this.numSSize.Maximum = new decimal(new int[] {
+            25,
+            0,
+            0,
+            0});
+            this.numSSize.Minimum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numSSize.Name = "numSSize";
+            this.numSSize.Size = new System.Drawing.Size(120, 22);
+            this.numSSize.TabIndex = 1;
+            this.numSSize.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 372);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Cell Size:";
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(206, 370);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 3;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(995, 369);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(75, 23);
+            this.btnGo.TabIndex = 4;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // btnAdvance
+            // 
+            this.btnAdvance.Location = new System.Drawing.Point(914, 369);
+            this.btnAdvance.Name = "btnAdvance";
+            this.btnAdvance.Size = new System.Drawing.Size(75, 23);
+            this.btnAdvance.TabIndex = 5;
+            this.btnAdvance.Text = "Advance";
+            this.btnAdvance.UseVisualStyleBackColor = true;
+            this.btnAdvance.Click += new System.EventHandler(this.btnAdvance_Click);
+            // 
+            // cboCells
+            // 
+            this.cboCells.FormattingEnabled = true;
+            this.cboCells.Location = new System.Drawing.Point(287, 369);
+            this.cboCells.Name = "cboCells";
+            this.cboCells.Size = new System.Drawing.Size(252, 24);
+            this.cboCells.TabIndex = 6;
+            // 
+            // ConwayMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1082, 403);
+            this.Controls.Add(this.cboCells);
+            this.Controls.Add(this.btnAdvance);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numSSize);
+            this.Controls.Add(this.pbGrid);
+            this.Name = "ConwayMain";
+            this.Text = "Conway\'s Game of Life";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ConwayMain_FormClosing);
+            this.Load += new System.EventHandler(this.ConwayMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbGrid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numSSize)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbGrid;
+        private System.Windows.Forms.NumericUpDown numSSize;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.Button btnAdvance;
+        private System.Windows.Forms.ComboBox cboCells;
+    }
+}
+
---
+++ Lab4/Lab4/Form1.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab4
+{
+    public partial class ConwayMain : Form
+    {
+        Boolean InProgress;
+        Grid CellGrid;
+
+        public ConwayMain()
+        {
+            InitializeComponent();
+        }
+
+        private void ConwayMain_Load(object sender, EventArgs e)
+        {
+            CreateGridSurface();
+            //GetActiveCounts();
+        }
+
+        private void CreateGridSurface()
+        {
+            Cell newCell;
+            Random random = new Random();
+
+            //Determine number of rows and columns in grid
+            int rows = (int)(pbGrid.Height / numSSize.Value);
+            int cols = (int)(pbGrid.Width / numSSize.Value);
+
+            //Create grid object
+            CellGrid = new Grid(rows, cols);
+
+            //Clear and rebuild the list of cells, activating 15% of cells at random
+            Grid.gridCells.Clear();
+
+            for (int y = 0; y < rows; y++)
+            {
+                for (int x = 0; x < cols; x++)
+                {
+                    newCell = new Cell(x, y, (int)numSSize.Value);
+                    newCell.IsAlive = (random.Next(100) < 15) ? true : false;
+                }
+            }
+
+            Grid.gridCells = Grid.gridCells.OrderBy(c => c.XPos).OrderBy(c => c.YPos).ToList();
+
+            UpdateGrid(CellGrid);
+        }
+
+        private void GetActiveCounts()
+        {
+            cboCells.Items.Clear();
+
+            foreach (Cell Cell in Grid.gridCells)
+            {
+                cboCells.Items.Add($"X:{Cell.XPos} ,Y:{Cell.YPos} , Count:{CellGrid.LiveAdjacent(Cell)} ");
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            CreateGridSurface();
+            //GetActiveCounts();
+        }
+
+        private void GetNextState()
+        {
+            //Calculate the next status of each cell
+            foreach (Cell cell in Grid.gridCells)
+            {
+                int activeCount = CellGrid.LiveAdjacent(cell);
+
+                if (cell.IsAlive)
+                {
+                    if ((activeCount < 2) || (activeCount > 3))
+                        cell.NextStatus = false;
+                    else
+                        cell.NextStatus = true;
+                }
+                else
+                {
+                    if (activeCount == 3)
+                        cell.NextStatus = true;
+                }
+            }
+
+            //for each cell, update IsAlive with NextStatus
+            foreach (Cell cell in Grid.gridCells)
+            {
+                cell.IsAlive = cell.NextStatus;
+            }
+
+            UpdateGrid(CellGrid);
+        }
+
+        private void UpdateGrid (Grid GridDisplay)
+        {
+            Cell newCell;
+            Random random = new Random();
+
+            //Create new image and update new picture box
+            using (Bitmap bmp = new Bitmap(pbGrid.Width, pbGrid.Height))
+            using (Graphics g = Graphics.FromImage(bmp))
+            using (SolidBrush cellBrush = new SolidBrush(Color.DarkOrange))
+            {
+                g.Clear(Color.Black);
+
+                foreach (Cell cell in Grid.gridCells)
+                {
+                    if (cell.IsAlive)
+                    {
+                        g.FillRectangle(cellBrush, cell.CellDisplay);
+                    }
+                }
+
+                if (pbGrid.Image!= null)
+                    pbGrid.Image.Dispose();
+
+                pbGrid.Image = (Bitmap)bmp.Clone();
+            }
+        }
+
+        private void btnAdvance_Click(object sender, EventArgs e)
+        {
+            GetNextState();
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            //Flip the in progress variable
+            InProgress = !InProgress;
+            
+            //Change the text in the button
+            btnGo.Text = InProgress ? "Stop" : "Go";
+
+            //While inProgress = true, continue to update grid
+            while (InProgress)
+            {
+                GetNextState();
+                Application.DoEvents();
+            }
+        }
+
+        private void ConwayMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            InProgress = false;
+            Application.Exit();
+        }
+    }
+
+    public class Grid
+    {
+        public static List<Cell> gridCells = new List<Cell>();
+
+        private int cRows;
+        private int cCols;
+
+        public Grid (int rows, int columns)
+        {
+            this.Rows = rows;
+            this.Columns = columns;
+        }
+
+        public int Rows
+        {
+            get { return cRows; }
+            set { cRows = value; }
+        }
+
+        public int Columns
+        {
+            get { return cCols; }
+            set { cCols = value; }
+        }
+
+        public int LiveAdjacent(Cell cell)
+        {
+            //Function to return number of active neighboring cells
+            //Use cell index numbers to search range of cells for active neighbors
+            //Sort cells grid collection by X and Y position to ensure accurate range
+            //Use for loop to search a range of cells within the collective including the rows immediately before and after the center cell rather than the entire grid
+
+            int liveAdjacent = 0;
+
+            //Get range of cells to be examined for active neighbors 
+            int cellIndex = (cell.YPos * this.Columns) + cell.XPos;
+            int startIndex = cellIndex - this.Columns - 2;
+            int endIndex = cellIndex + this.Columns + 2;
+
+            //Ensure that the start and end indexes don't exceed the grid range
+            startIndex = (startIndex < 0) ? 0 : startIndex;
+            endIndex = (endIndex > (Grid.gridCells.Count - 1)) ? Grid.gridCells.Count - 1 : endIndex;
+
+            //Iterate through the defined range and look for active neighbors
+            for (int x = startIndex; x < endIndex; x++)
+            {
+                if (Math.Abs(cell.XPos - gridCells[x].XPos) < 2 && Math.Abs(cell.YPos - gridCells[x].YPos) < 2)
+                {
+                    if (Grid.gridCells[x].Location != cell.Location)
+                    {
+                        if (gridCells[x].IsAlive)
+                        {
+                            liveAdjacent++;
+                        }
+                    }
+                }
+            }
+
+            return liveAdjacent;
+        }
+    }
+
+    public class Cell
+    {
+        private Point clocation;
+        private Rectangle cCellDisplay;
+        private Size cCellSize;
+        private int cXPos;
+        private int cYPos;
+        private Boolean cIsAlive;
+        private Boolean cNext;
+
+        public Cell(Point location, int X, int Y)
+        {
+            int cellSize;
+            //Set object settings and add to grid
+            this.Location = location;
+            this.YPos = Y;
+            this.XPos = X;
+            Grid.gridCells.Add(this);
+            //If location is not 0, divide pixel location by grid location to get size of the cell
+            cellSize = (X == 0) ? 0 : location.X / X;
+
+            // Create rectangle to display as needed using calculated cell size
+            this.cCellDisplay = new Rectangle(this.Location, new Size(cellSize, cellSize));
+        }
+
+        public Cell(int X, int Y, int CellSize)
+        {
+            this.Location = new Point(X * CellSize, Y * CellSize);
+            this.XPos= X;
+            this.YPos= Y;
+            this.CellSize = new Size(CellSize, CellSize);
+            Grid.gridCells.Add(this);
+            this.CellDisplay = new Rectangle(this.Location, new Size(CellSize - 1, CellSize - 1));
+        }
+
+        public Rectangle CellDisplay
+        {
+            //Rectangle object for displaying cell when needed
+            get { return cCellDisplay; }
+            set { cCellDisplay = value; }
+        }
+
+        public Size CellSize
+        {
+            //Calcualte cell size
+            get { return cCellSize; }
+            set { cCellSize = value; }
+        }
+
+        public Point Location
+        {
+            //X, Y Point that specifies cell location
+            get { return clocation; }
+            set { clocation = value; }
+        }
+
+        public int XPos
+        {
+            get { return cXPos; }
+            set { cXPos = value; }
+        }
+
+        public int YPos
+        {
+            get { return cYPos; }
+            set { cYPos = value; }
+        }
+
+        public Boolean IsAlive
+        {
+            get { return cIsAlive; }
+            set { cIsAlive = value; }
+        }
+
+        public Boolean NextStatus
+        {
+            get { return cNext; }
+            set { cNext = value; }
+        }
+
+        public override string ToString()
+        {
+            //Override the cell ToString to provide location information
+            return $"GridX:{this.XPos} GridY:{this.YPos} LocX:{this.Location.X} LocY:{this.Location.Y}";
+        }
+    }
+}
